60cfbc1a03b66fffe2372ca88d0ca50a2d407f83
null <dwsong07@naver.com>
2021-08-29 03:24:15 +0900

    Add try catch in ipc

--- electron/ipc.ts
+++ electron/ipc.ts
@@ -3,10 +3,16 @@ import ytsr from "ytsr";
 
 export default function () {
     ipcMain.on("YTDL_VIDEO_SEARCH", async (e, text: string) => {
-        const searchResult = await ytsr(text, {
-            pages: 1,
-        });
+        try {
+            if (!text) return;
 
-        e.reply("YTDL_VIDEO_SEARCH_REPLY", searchResult);
+            const searchResult = await ytsr(text, {
+                pages: 1,
+            });
+
+            e.reply("YTDL_VIDEO_SEARCH_REPLY", searchResult);
+        } catch (err) {
+            console.error(err);
+        }
     });
 }
